dc2103a4519abc744d37a394cebc3f9b7954f566
KIRVAS <133602486+KIRVASgames@users.noreply.github.com>
2024-10-30 20:08:39 +0300

    Add characterEnabler.cs

---
+++ Assets/_Project/Scripts/UI/CharacterEnabler.cs
@@ -0,0 +1,41 @@
+using CthulhuGame;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterEnabler : MonoBehaviour 
+{
+    [SerializeField] private GameObject[] _characters;
+    [SerializeField] private Button _closeButton;
+
+    private void Start()
+    {
+        DeactivateAllCharacters();
+
+        _closeButton.onClick.AddListener(DeactivateAllCharacters);
+        ActionButton.Instance.OnActionButtonClicked += ActivateRandomCharacter;
+    }
+
+    private void OnDisable()
+    {
+        DeactivateAllCharacters();
+    }
+
+    private void OnDestroy()
+    {
+        _closeButton.onClick.RemoveListener(DeactivateAllCharacters);
+        ActionButton.Instance.OnActionButtonClicked -= ActivateRandomCharacter;
+    }
+
+    private void ActivateRandomCharacter()
+    {
+        _characters[Random.Range(0, _characters.Length)].SetActive(true);
+    }
+
+    private void DeactivateAllCharacters()
+    {
+        foreach (var character in _characters)
+        {
+            character.SetActive(false);
+        }
+    }
+}
